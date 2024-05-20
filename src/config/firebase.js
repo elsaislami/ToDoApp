60f3e6c4a93131404903b0105867748744d31d61
robertocemeri <elsaislami23@gmail.com>
2024-05-20 18:13:16 +0200

    small fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -18,26 +18,3 @@ if (!firebase.apps.length) {
 
 export const auth = firebase.auth();
 
-
-// import firebase from '@react-native-firebase/app';
-// import '@react-native-firebase/auth';
-// import '@react-native-firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCBrWRdbNL0aI3J3CgmdBejuT_zXG2ZWHk",
-//   authDomain: "todoapp-a5d99.firebaseapp.com",
-//   projectId: "todoapp-a5d99",
-//   storageBucket: "todoapp-a5d99.appspot.com",
-//   messagingSenderId: "96912910192",
-//   appId: "1:96912910192:ios:d0aef9b14e80c7288611b5",
-// };
-
-// let app;
-// if (firebase.apps.length === 0) {
-//     app = firebase.initializeApp(firebaseConfig )
-// } else {
-//     app = firebase.app()
-// }
-
-// const db = firebase.firestore();
-// const auth = firebase.auth();
